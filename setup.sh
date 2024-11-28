ffc46e0a47d3f8e1dea6927b1bee54a52da7cba2
Paul Projects <paulciurean2505@gmail.com>
2024-11-28 10:56:12 +0000

    2nd commit

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ sudo apt update
 sudo apt install nginx -y
 sudo systemctl start nginx
 sudo systemctl enable nginx
-sudo sed -i 's/Welcome to nginx/Welcome To BabyGroot's website/g' /usr/share/nginx/html/index.html
+sudo sed -i "s/Welcome to nginx/Welcome To BabyGroot's website/g" /usr/share/nginx/html/index.html
 sudo systemctl restart nginx
 
