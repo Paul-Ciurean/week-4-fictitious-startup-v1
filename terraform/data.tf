d965b17a7e0cb8afb3b46cb319b427f19b711909
Paul Projects <paulciurean2505@gmail.com>
2024-11-29 13:07:30 +0000

    test

--- terraform/data.tf
+++ terraform/data.tf
@@ -3,22 +3,11 @@
 ########
 
 data "aws_ami" "ubuntu" {
-  executable_users = ["self"]
   most_recent      = true
   owners           = ["self"]
 
   filter {
     name   = "name"
     values = ["Groot-startup-${var.custom_ami_version}"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
   }
 }
