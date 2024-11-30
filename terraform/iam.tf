7359ac3bc1a9e04e34d88ab8f37d0f0054f6be87
Paul Projects <paulciurean2505@gmail.com>
2024-11-30 10:26:55 +0000

    added instance profile

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -3,21 +3,8 @@ resource "aws_iam_instance_profile" "instance_profile" {
   role = aws_iam_role.role.name
 }
 
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
 resource "aws_iam_role" "role" {
-  name               = "role_for_ssm"
+  name               = "instance_profile_role"
   path               = "/"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  assume_role_policy = file("InstanceRole.json")
 }
