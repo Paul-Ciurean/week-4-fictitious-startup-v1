c8e77bf29e3cf4fb1261498711a3fdbb36201d16
Paul Projects <paulciurean2505@gmail.com>
2024-11-30 10:44:42 +0000

    added instance profile

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -5,6 +5,21 @@ resource "aws_iam_instance_profile" "instance_profile" {
 
 resource "aws_iam_role" "role" {
   name               = "instance_profile_role"
-  path               = "/"
-  assume_role_policy = file("InstanceRole.json")
+  assume_role_policy = {
+    assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "name" {
+  role = aws_iam_role.role.name
+  policy_arn = var.role_ssm
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,8 @@
 variable "custom_ami_version" {
   description = "The custom AMI version"
 }
+
+variable "role_ssm" {
+  description = "The role for SSM"
+  default = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
